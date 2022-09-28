efb70285dbf9230661c3c00e5bbe0570d243111a
pauline-wanjiku <paulinendungushiko96@gmail.com>
2022-09-28 16:08:02 +0300

    First commit

---
+++ quote-generator/src/apikey.js
@@ -0,0 +1,10 @@
+export const options = {
+  method: "POST",
+  url: "https://motivational-quotes1.p.rapidapi.com/motivation",
+  headers: {
+    "content-type": "application/json",
+    "X-RapidAPI-Key": "3637e661bdmshc4f3ffe62622951p169b0fjsn69f8f1c02781",
+    "X-RapidAPI-Host": "motivational-quotes1.p.rapidapi.com",
+  },
+  data: '{"key1":"value","key2":"value"}',
+};
